3217a5d8d6575a0fe3e1eaf166c1900e3e0aa5d7
Kashish-max <kashish24102001@gmail.com>
2020-09-24 18:23:09 +0530

    Admin page activated

--- game/admin.py
+++ game/admin.py
@@ -1,4 +1,29 @@
 from django.contrib import admin
-# from .models import Question
 
-# admin.site.register()
+from .models import blogPost
+
+admin.site.register(blogPost)
+
+# class ChoiceInline(admin.TabularInline):  # StackedInline, TabularInline
+#     model = Choice
+#     extra = 3
+
+
+# class blogPostAdmin(admin.ModelAdmin):
+#     # fields = ['pub_date', 'question_text']
+#     fieldsets = [
+#         (None,               {'fields': ['post_title']}),
+#         ('Date information', {'fields': [
+#          'pub_date'], 'classes': ['collapse']}),
+#     ]
+#     # inlines = [ChoiceInline]
+
+#     # displaying columns in the questions list
+#     list_display = ('post_title', 'pub_date')
+
+#     # additional features
+#     # list_filter = ['pub_date']
+#     search_fields = ['post_title']
+
+
+# admin.site.register(blogPost, blogPostAdmin)
--- game/views.py
+++ game/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from game.models import Question
+from django.core.paginator import Paginator
+
+from game.models import blogPost
 
 
 def home(request):
@@ -8,7 +10,15 @@ def home(request):
 
 
 def news(request):
-    return render(request, 'game/news.html')
+    blog_list = blogPost.objects.all()
+    paginator = Paginator(blog_list, 3)  # Show 3 posts per page
+
+    page = request.GET.get('page')
+    blogs = paginator.get_page(page)
+    context = {
+        'blogs': blogs,
+    }
+    return render(request, 'game/news.html', context)
 
 
 def newsPage(request):
